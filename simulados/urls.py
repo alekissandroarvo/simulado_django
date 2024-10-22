e7ad391b0f97375c19c027c343d4279f4318d399
Alekissandro Oliveira <alek.rossi@gmail.com>
2024-10-22 09:52:19 -0300

    changes settings urls wsgi to check css

--- simulados/urls.py
+++ simulados/urls.py
@@ -16,8 +16,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.urls.static import static
+
+
+from simulados import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('simuladosingles.urls')),
+]
+
+urlpatterns += [
+    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
+    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
+
 ]
