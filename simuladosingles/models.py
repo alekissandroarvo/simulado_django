7a8f53a24eaa0fbca10c565e6cff8ad9a2b68d8a
Alekissandro Oliveira <alek.rossi@gmail.com>
2024-10-25 11:09:26 -0300

    concluído resposta

--- simuladosingles/models.py
+++ simuladosingles/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Question(models.Model):
-    pergunta = models.CharField(max_length=200)
-    assertiva1 = models.CharField(max_length=200)
-    assertiva2 = models.CharField(max_length=200)
-    assertiva3 = models.CharField(max_length=200)
-    assertiva4 = models.CharField(max_length=200)
-    resposta = models.IntegerField()
+    pergunta = models.CharField(max_length=500)
+    assertiva1 = models.CharField(max_length=500)
+    assertiva2 = models.CharField(max_length=500)
+    assertiva3 = models.CharField(max_length=500)
+    assertiva4 = models.CharField(max_length=500)
+    resposta = models.CharField(max_length=500)
--- simuladosingles/views.py
+++ simuladosingles/views.py
@@ -18,21 +18,24 @@ def questionario(request):
 
 def resultado_questionario(request):
     question_list= Question.objects.order_by("id")
-    answer_list_right=[]
-    answer_list_wrong=[]    
-    assertiva_right=[]
-    assertiva_wrong=[]
+    answer_list_right_id=[]
+    answer_list_wrong_assertiva=[]
+    answer_list_right_assertiva=[]
+    answer_list_wrong_id=[]
     i=1
+    
     for answer in question_list:
-        if answer.resposta == int(request.GET[str(i)]):
-            answer_list_right.append( answer.id) 
+        if answer.resposta == request.GET[str(i)]:
+            answer_list_right_id.append( answer.id)             
         else:
-            answer_list_wrong.append( answer.id)
+            answer_list_wrong_id.append( answer.id)  
+            answer_list_wrong_assertiva.append( request.GET[str(i)])
+            answer_list_right_assertiva.append( answer.resposta)          
         i=i+1    
     context = {
-        "answer_list_right": answer_list_right,
-        "answer_list_wrong": answer_list_wrong,
-        "percent": (len(answer_list_right)/len(question_list))*100,
+        "answer_list_right": answer_list_right_id,
+        "answer_list_wrong": zip(answer_list_wrong_id,answer_list_wrong_assertiva,answer_list_right_assertiva),
+        "percent": (len(answer_list_right_id)/len(question_list))*100,
               
     }
     return render(request, 'resultado_questionario.html',context)
